a7b5cad92f1f590a1827bf78df50590a2b030592
danielsky <karma0rg4u@gmail.com>
2018-10-19 20:58:07 +0100

    Finalizing exercise

--- js/script.js
+++ js/script.js
@@ -1,6 +1,21 @@
 //waits until page is ready
 $(document).ready(function() {
  
+    $(".card_image").click(function() {
+        $(this).next().children("p").slideToggle("fast");
+    });        
+        
+    $(".card").click(function() {
+        $(this).toggleClass("card_bkg");
+    });
+        
+    $("#select_btn").click(function() {
+        $(".card").not(".card_bkg").hide();
+    });
+    
+    $("#all_btn").click(function() {
+        $(".card").css("display", "inline-block");
+    });
 
 
 
